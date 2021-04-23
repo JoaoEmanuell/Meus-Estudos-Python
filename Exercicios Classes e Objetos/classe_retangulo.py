137c72d8f0a8dadee3fa0885308acdbe949c5d25
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-23 19:06:42 -0300

    def lados

--- Exercicios Classes e Objetos/classe_retangulo.py
+++ Exercicios Classes e Objetos/classe_retangulo.py
@@ -9,4 +9,14 @@ class ret():
         self.comprimento = comprimento
         self.largura = largura
         
+    def lados(self):
+       self.comprimento = float(input("Qual o comprimento? "))
+       self.largura = float(input("Qual a largura? "))
+       retorno = (print(f"O valor do comprimento é {self.comprimento} e o da largura é {self.largura}\nA area do retangulo é {self.comprimento * self.largura}\nO perimentro do retangulo é igual a {(self.comprimento * 2) + (self.largura * 2)}"))
+       return retorno
+       
+   
+
 p1 = ret(2, 6)
+
+p1.lados()
