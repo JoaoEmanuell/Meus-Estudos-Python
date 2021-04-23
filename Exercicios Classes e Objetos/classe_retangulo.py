0ac3ede26e7011fb3eaf8b6ad353df2d4cd2cdb2
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-23 19:23:37 -0300

    classe_retangulo feito

--- Exercicios Classes e Objetos/classe_retangulo.py
+++ Exercicios Classes e Objetos/classe_retangulo.py
@@ -14,9 +14,19 @@ class ret():
        self.largura = float(input("Qual a largura? "))
        retorno = (print(f"O valor do comprimento é {self.comprimento} e o da largura é {self.largura}\nA area do retangulo é {self.comprimento * self.largura}\nO perimentro do retangulo é igual a {(self.comprimento * 2) + (self.largura * 2)}"))
        return retorno
-       
-   
 
-p1 = ret(2, 6)
+class casa(ret):
+    def __init__(self, comprimento=1, largura=1):
+        self.comprimento = comprimento
+        self.largura = largura
+    
+    def lados(self):
+       self.comprimento = float(input("Qual o comprimento do lugar? "))
+       self.largura = float(input("Qual a largura do lugar? "))
+       self.m2 = self.comprimento * self.largura
+       retorno = (print(f"Serão necessarios {int(self.m2)} pisos para completar o lugar"))
+       return retorno
+        
+p1 = casa()
 
 p1.lados()
