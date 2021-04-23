1000bd025aeeacde2891ffd85b287f0067f9b554
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-04-23 18:51:41 -0300

    ret, init

---
+++ Exercicios Classes e Objetos/classe_retangulo.py
@@ -0,0 +1,12 @@
+class ret():
+    def __init__(self, comprimento, largura):
+        """[função de inicialização]
+
+        Args:
+            comprimento ([float]): [Comprimento do retangulo]
+            largura ([float]): [Comprimento do retangulo]
+        """
+        self.comprimento = comprimento
+        self.largura = largura
+        
+p1 = ret(2, 6)
