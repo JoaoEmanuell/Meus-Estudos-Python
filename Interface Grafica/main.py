d0a68ddad273c9913ca5d226f20ad0c7cf74196d
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-05-09 13:39:59 -0300

    main update

--- Interface Grafica/main.py
+++ Interface Grafica/main.py
@@ -7,6 +7,8 @@ class TelaPython:
             [sg.Text('Idade', size = (5,0)),sg.Input(size = (3,0), key = 'idade')],
             [sg.Text('Quais provedores de e-mail serão aceitos? ')],
             [sg.Checkbox('Gmail', key = 'gmail'), sg.Checkbox('Outlook', key = 'outlook'), sg.Checkbox('Yahoo', key = 'yahoo')], #Checkbox cria uma caixa para você clicar
+            [sg.Text('Aceita cartão')],
+            [sg.Radio('Sim', 'cartoes', key = 'cartao_sim'), sg.Radio('Não', 'cartoes', key = 'cartao_não')],
             [sg.Button('Enviar dados')]
         ]
         
@@ -15,8 +17,20 @@ class TelaPython:
         self.button, self.values = janela.Read() #isso aqui servirar para extrair os dados da janela para o codigo
         
     def Iniciar(self):
-        print(self.values)
-
+        nome = self.values['nome'] # recebe o item que possue a key nome, do dicionario valores
+        idade = self.values['idade']
+        gmail = self.values['gmail']
+        outlook = self.values['outlook']
+        yahoo = self.values['yahoo']
+        cartao_sim = self.values['cartao_sim']
+        cartao_nao = self.values['cartao_não']
+        print(f'nome: {nome}')
+        print(f'idade: {idade}')
+        print(f'aceita gmail: {gmail}')
+        print(f'aceita outlook: {outlook}')
+        print(f'aceita yahoo: {yahoo}')
+        print(f'aceita cartao, sim: {cartao_sim}')
+        print(f'aceita cartao, não: {cartao_nao}')
 tela = TelaPython()#var tela 
 
 tela.Iniciar()#inicia a tela
