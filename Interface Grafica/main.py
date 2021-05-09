c8211ca10ebc11d1d48f64be3e23fcd9b988f7c6
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-05-09 13:55:55 -0300

    main update, txt update

--- Interface Grafica/main.py
+++ Interface Grafica/main.py
@@ -2,6 +2,7 @@ import PySimpleGUI as sg
 
 class TelaPython:
     def __init__(self):
+        sg.theme(new_theme = 'DarkBrown4')# define o tema da janela, por padrão o tema sera none, no site da documentação há uma lista de temas.
         layout = [#layout da pagina
             [sg.Text('Nome', size = (5,0)),sg.Input(size = (50,0), key = 'nome')], #cada lista dessa é uma linha da janela
             [sg.Text('Idade', size = (5,0)),sg.Input(size = (3,0), key = 'idade')],
@@ -9,7 +10,7 @@ class TelaPython:
             [sg.Checkbox('Gmail', key = 'gmail'), sg.Checkbox('Outlook', key = 'outlook'), sg.Checkbox('Yahoo', key = 'yahoo')], #Checkbox cria uma caixa para você clicar
             [sg.Text('Aceita cartão')],
             [sg.Radio('Sim', 'cartoes', key = 'cartao_sim'), sg.Radio('Não', 'cartoes', key = 'cartao_não')],
-            [sg.Slider(range = (0, 100), default_value = 0, orientation = 'h', size = (15, 20), key = 'slider')] #Slider, range será o valor minimo e maximo que ele ira receber, default_value é o valor padrão, orientation = 'h' ou 'v', h = 'horizontal', v = 'vertical'
+            [sg.Slider(range = (0, 100), default_value = 0, orientation = 'h', size = (15, 20), key = 'slider')],#Slider, range será o valor minimo e maximo que ele ira receber, default_value é o valor padrão, orientation = 'h' ou 'v', h = 'horizontal', v = 'vertical'
             [sg.Button('Enviar dados')],
             [sg.Output(size = (30,20))] #output, tudo que seria printado no console é enviado para essa aréa
         ]
@@ -28,6 +29,7 @@ class TelaPython:
             yahoo = self.values['yahoo']
             cartao_sim = self.values['cartao_sim']
             cartao_nao = self.values['cartao_não']
+            slider = self.values['slider']
             print(f'nome: {nome}')
             print(f'idade: {idade}')
             print(f'aceita gmail: {gmail}')
@@ -35,6 +37,7 @@ class TelaPython:
             print(f'aceita yahoo: {yahoo}')
             print(f'aceita cartao, sim: {cartao_sim}')
             print(f'aceita cartao, não: {cartao_nao}')
+            print(f'slider: {slider}')
 
 tela = TelaPython()#var tela 
 
