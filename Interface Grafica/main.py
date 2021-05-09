0e8f58310753a249f1f09616ca27184132f4b267
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-05-09 13:28:07 -0300

    main update, txt updaate

--- Interface Grafica/main.py
+++ Interface Grafica/main.py
@@ -3,8 +3,10 @@ import PySimpleGUI as sg
 class TelaPython:
     def __init__(self):
         layout = [#layout da pagina
-            [sg.Text('Nome'),sg.Input()], #cada lista dessa é uma linha da janela
-            [sg.Text('Idade'),sg.Input()],
+            [sg.Text('Nome', size = (5,0)),sg.Input(size = (50,0), key = 'nome')], #cada lista dessa é uma linha da janela
+            [sg.Text('Idade', size = (5,0)),sg.Input(size = (3,0), key = 'idade')],
+            [sg.Text('Quais provedores de e-mail serão aceitos? ')],
+            [sg.Checkbox('Gmail', key = 'gmail'), sg.Checkbox('Outlook', key = 'outlook'), sg.Checkbox('Yahoo', key = 'yahoo')], #Checkbox cria uma caixa para você clicar
             [sg.Button('Enviar dados')]
         ]
         
