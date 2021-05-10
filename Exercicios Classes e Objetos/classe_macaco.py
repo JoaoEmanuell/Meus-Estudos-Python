c03607ba3ddc234de1acef81a0c7d13b681ec3e1
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-05-10 14:26:35 -0300

    class_macaco docstrings update

--- Exercicios Classes e Objetos/classe_macaco.py
+++ Exercicios Classes e Objetos/classe_macaco.py
@@ -2,11 +2,21 @@
 """
     
 class macaco():
+    """[classe que modela um macaco]
+    """    
     def __init__(self, nome, estomago = 'banana'):
+        """[init]
+
+        Args:
+            nome ([str]): [Nome do macaco]
+            estomago (str, optional): [estomago do macaco, por padrão o macaco vem com uma banana dentro do estomago]. Defaults to 'banana'.
+        """        
         self.nome = nome
         self.estomago = [estomago]
     
     def comer(self):
+        """[serve para dar um alimento para o macaco, pergunte qual o alimento que o macaco ira comer, adicione ao estomago, escreva que foi um Sucesso, pergunte se o usuario quer sair.]
+        """        
         while True:
             comida = str(input(f"O que o {self.nome} ira comer? "))
             self.estomago.append(comida)
@@ -16,6 +26,8 @@ class macaco():
                 break
         
     def ver_estomago(self):
+        """[serve para verificar o estomago do macaco, caso o len do estomago seja 0, escreva que o estomago está vazio, senão, escreva, para cada item no estomago, escreva o item e separe por ,]
+        """        
         if len(self.estomago) == 0:
             print(f'O estomago de {self.nome} está vazio')
         else:
@@ -24,12 +36,19 @@ class macaco():
                 print(f'{i},',end=' ')
 
     def digerir(self):
+        """[serve para digerir os alimentos guardados no estomago, escreva, para cada item em estomago, escreva "digerindo" {item} e remova do estomago o item.]
+        """        
         print('Digerindo os alimentos:')
         for i in self.estomago:
             print(f'Digerindo {i}')
             self.estomago.remove(i)
 
 class macaco1(macaco):
+    """[classe que herda as mesmas caracteristicas da classe macaco]
+
+    Args:
+        macaco ([class]): [classe macaco]
+    """    
     pass
 
 mamaco = macaco('Simba')
