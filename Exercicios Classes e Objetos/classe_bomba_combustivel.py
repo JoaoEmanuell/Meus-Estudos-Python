a050b66f43ec801448cd4f3ad8077baa7271d21f
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-05-11 17:33:33 -0300

    abastecerPorValor

--- Exercicios Classes e Objetos/classe_bomba_combustivel.py
+++ Exercicios Classes e Objetos/classe_bomba_combustivel.py
@@ -1,4 +1,5 @@
-    """[10 - 
+"""
+[10 - 
     Faça um programa completo utilizando classes e métodos que:
 a - Possua uma classe chamada bombaCombustível, com no mínimo esses atributos:
     i   - tipoCombustivel.
@@ -7,15 +8,29 @@ a - Possua uma classe chamada bombaCombustível, com no mínimo esses atributos:
     
 b - Possua no mínimo esses métodos:
     i   -  abastecerPorValor( ) – método onde é informado o valor a ser abastecido e mostra a quantidade de litros que foi colocada no veículo
-    ii  -  abastecerPorLitro( ) – método onde é informado a quantidade em litros de    combustível e mostra o valor a ser pago pelo cliente.
+    ii  -  abastecerPorLitro( ) – método onde é informado a quantidade em litros de combustível e mostra o valor a ser pago pelo cliente.
     iii -  alterarValor( ) – altera o valor do litro do combustível.
     iv  -  alterarCombustivel( ) – altera o tipo do combustível.
     v   - alterarQuantidadeCombustivel( ) – altera a quantidade de combustível restante na bomba.
     OBS: Sempre que acontecer um abastecimento é necessário atualizar a quantidade de combustível total na bomba.]
-    """
+"""
     
 class bomba:
     def __init__(self, tipoCombustivel = 'Gasolina', valorLitro = 5.0, quantidadeCombustivel = 100.0):
         self.combustivel = tipoCombustivel
         self.preço = valorLitro
         self.quantidade = quantidadeCombustivel
+    
+    def abastecerPorValor(self):
+        valor = float(input(f"Quantos reais de {self.combustivel} você deseja abastecer? "))
+        tot = valor / self.preço
+        if tot > self.quantidade:
+            print(f"Desculpe, mas a bomba não tem {self.combustivel} o suficiente")
+            return(False)
+        else:
+            print(f"Sucesso, você abasteceu {tot} litros de {self.combustivel}")
+            return(True)
+       
+posto = bomba()
+
+posto.abastecerPorValor()
