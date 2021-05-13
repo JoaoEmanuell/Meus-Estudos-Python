30bf16162f2d7534a23447d0a432e6a67b0214e4
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-05-13 15:17:52 -0300

    Gerador

--- Interface Grafica/gerador_de_senhas.py
+++ Interface Grafica/gerador_de_senhas.py
@@ -1,15 +1,16 @@
 #Imports
 import PySimpleGUI as sg
+from random import randint
 
 #Interface
 class interface():
     def __init__(self):
         layout = [
             [sg.Text("GERADOR DE SENHAS", justification = 'center')],
-            [sg.Text("Tamanho"), sg.Input(size = (20,0))],
-            [sg.Checkbox("Letras minusculas", key = 'Letras minusculas')],
-            [sg.Checkbox("Numeros")],
-            [sg.Checkbox("Caracteres especiais (#$?!)")],
+            [sg.Text("Tamanho"), sg.Input(size = (20,0), key = 'Tam')],
+            [sg.Checkbox("Letras minusculas", key = 'Min')],
+            [sg.Checkbox("Numeros", key = 'Nums')],
+            [sg.Checkbox("Caracteres especiais (#$?!)", key = 'Esp')],
             [sg.Button("Gerar senha")],
             [sg.Output(size = (50, 5))]
         ]
@@ -19,8 +20,25 @@ class interface():
     def Iniciar(self):
         while True:
             self.button, self.values = self.janela.Read()
+            self.tamanho = self.values['Tam']
+            self.minusculas = self.values['Min']
+            self.numeros = self.values['Nums']
+            self.especiais = self.values['Esp']
+            '''print(int(self.tamanho))
+            print(self.minusculas)
+            print(self.numeros)
+            print(self.especiais)'''
 #Gerador
-
+class Gerador(interface):
+    def __init__(self):
+        self.Minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.Maisculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        self.Numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        self.Especiais = ['?', ';', ',', '.', '!', '{', '}', '(', ')', '/', '*', '-', '+']
+    
+    def geração(self):
+        limite = int(self.numeros)
+        
 #Progama
 
 tela = interface()
