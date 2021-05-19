8f606d02673463283f71c1296ef7d3b7bbecddd2
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-05-19 18:08:55 -0300

    interface criada, progama utilizavel

--- Interface Grafica/tree.py
+++ Interface Grafica/tree.py
@@ -1,6 +1,8 @@
 from time import sleep
+import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import Tree
 
-def tempo(t):
+def tempo(t, janela, chave):
     """[Função para contar o tempo, serve como o nome já diz para contar o tempo que você irar passar]
 
     Args:
@@ -9,6 +11,7 @@ def tempo(t):
     minutos = int(t)
     segundos = 60
     while True:
+        janela.FindElement(chave).Update('')
         texto = '{:02d}:{:02d}'.format(minutos, segundos)
         if minutos < 0:
             break
@@ -22,4 +25,22 @@ def tempo(t):
         print(texto, end = "\r")
         sleep(0.15)
 
-t = tempo(1)
+class interface():
+    def __init__(self):
+        layout = [
+            [sg.Text("Minutos: ") ,sg.Input(size = '2', key = "Tempo")],
+            [sg.Button("Iniciar")],
+            [sg.Output(key = "saida")]
+        ]
+        
+        self.janela = sg.Window('Tree').layout(layout)
+    def iniciar(self):
+        while True:
+            self.button, self.values = self.janela.Read()
+            minutos = self.values["Tempo"]
+            tempo(minutos, self.janela, 'saida')
+            print("Encerrado")
+    
+tela = interface()
+
+tela.iniciar()
