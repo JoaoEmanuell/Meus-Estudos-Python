42bb3cd56775f8fd5184c741d09bfc4fc465dab6
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-06-29 19:58:15 -0300

    codigo do criptiografador simplificado

--- Projetos/Cripitografador/cript.py
+++ Projetos/Cripitografador/cript.py
@@ -11,7 +11,7 @@ Esse projeto se trata de um cripitografador de mensagens, nos teclados de celula
 9 | w = 9; x = 99; y = 999; z = 9999;
 '''
 
-texto = 'Eae vei blz?'
+texto = 'Oi, tudo bem?'
 def criptiografador(texto):
     def grupo3(l, a, b, c, a1, b1, c1):
             if (l == a):
@@ -20,77 +20,42 @@ def criptiografador(texto):
                 return(b1)
             else:
                 return(c1)
+    def grupo4(l, a, b, c, d, a1, b1, c1, d1):
+            if (l == a):
+                return(a1)
+            elif(l == b):
+                return(b1)
+            elif(l == c):
+                return(c1)
+            else:
+                return(c1)
     txt = texto.lower()
     conver = []
     for l in txt:
     #Grupo dos 2
         if (l == 'a' or l == 'b' or l == 'c'):
-            if (l == 'a'):
-                conver.append(2)
-            elif(l == 'b'):
-                conver.append(22)
-            else:
-                conver.append(222)
+            conver.append(grupo3(l, 'a', 'b', 'c', 2, 22, 222))
     #Grupo dos 3
         elif (l == 'd' or l == 'e' or l == 'f'):
-            if (l == 'd'):
-                conver.append(3)
-            elif(l == 'e'):
-                conver.append(33)
-            else:
-                conver.append(333)
+            conver.append(grupo3(l, 'd', 'e', 'f', 3, 33, 333))
     #Grupo dos 4
         elif (l == 'g' or l == 'h' or l == 'i'):
-            if (l == 'g'):
-                conver.append(4)
-            elif(l == 'h'):
-                conver.append(44)
-            else:
-                conver.append(444)
+            conver.append(grupo3(l, 'g', 'h', 'i', 4, 44, 444))
     #Grupo dos 5
         elif (l == 'j' or l == 'k' or l == 'l'):
-            if (l == 'j'):
-                conver.append(5)
-            elif(l == 'k'):
-                conver.append(55)
-            else:
-                conver.append(555)
+            conver.append(grupo3(l, 'j', 'k', 'l', 5, 55, 555))
     #Grupo dos 6
         elif (l == 'm' or l == 'n' or l == 'o'):
-            if (l == 'm'):
-                conver.append(6)
-            elif(l == 'n'):
-                conver.append(66)
-            else:
-                conver.append(666)
+            conver.append(grupo3(l, 'm', 'n', 'o', 6, 66, 666))
     #Grupo dos 7
         elif (l == 'p' or l == 'q' or l == 'r' or l == 's'):
-            if (l == 'p'):
-                conver.append(7)
-            elif(l == 'q'):
-                conver.append(77)
-            elif(l == 'r'):
-                conver.append(777)
-            else:
-                conver.append(7777)
+            conver.append(grupo4(l, 'p', 'q', 'r', 's', 7, 77, 777, 7777))
     #Grupo dos 8
         elif (l == 't' or l == 'u' or l == 'v'):
-            if (l == 't'):
-                conver.append(8)
-            elif(l == 'u'):
-                conver.append(88)
-            else:
-                conver.append(888)
+            conver.append(grupo3(l, 't', 'u', 'v', 8, 88, 888))
     #Grupo dos 9
         elif (l == 'w' or l == 'x' or l == 'y' or l == 'z'):
-            if (l == 'w'):
-                conver.append(9)
-            elif(l == 'x'):
-                conver.append(99)
-            elif(l == 'y'):
-                conver.append(999)
-            else:
-                conver.append(9999)
+            conver.append(grupo4(l, 'w', 'x', 'y', 'z', 9, 99, 999, 9999))
     #Espaços
         elif (l == ' '):
             conver.append('')
