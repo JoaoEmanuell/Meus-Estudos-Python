07d9374c8c11b84f93ca093b0338f87980daebf6
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-10-04 17:32:02 -0300

    Interface concluida

--- Projetos/Download-music-youtube/source/interface.py
+++ Projetos/Download-music-youtube/source/interface.py
@@ -3,12 +3,10 @@ import PySimpleGUI as sg
 class Interface():
     def __init__(self) -> None:
         layout = [
-            [sg.Radio('CRIPTOGRAFAR', 'select', key='encrypt', default=True), sg.Radio('DESCRIPTOGRAFAR', 'select', key='decrypt')],
-            [sg.Text('Texto de entrada')],
-            [sg.Input(key = 'entrada', size=(80, 20))],
-            [sg.Button('REALIZAR OPERAÇÃO')],
-            [sg.Text('Saida')],
-            [sg.Output(size = (80, 10), key='output')]
+            [sg.Text('Link da Música', key='playlist')],
+            [sg.Input(key = 'link', size=(80, 20))],
+            [sg.Button('Baixar Música')],
+            [sg.Output(size = (80, 5), key='output')]
         ]
     
         self.janela = sg.Window('MUSIC-DOWNLOAD').layout(layout)
@@ -16,13 +14,14 @@ class Interface():
     def start(self):
         while True:
             self.button, self.values = self.janela.Read()
-            self.entrada = self.values['entrada']
-            self.encrypt = self.values['encrypt']
-            self.decrypt = self.values['decrypt']
-            Interface.clear(self.janela, 'output')
+            self.link = self.values['link']
+            print(self.link)
     
-    def clear(janela, chave):
-        janela.FindElement(chave).Update('')
+    def clear(self, key):
+        self.janela.FindElement(key).Update('')
+
+    def write(self, key, text):
+        self.janela.FindElement(key).Update(text)
 
 tela = Interface()
 tela.start()
