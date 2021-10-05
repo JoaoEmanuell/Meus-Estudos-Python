c70c3d723f6b1490eff708909a13c13ec169a576
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-10-05 19:16:48 -0300

    event quit and event for download music

--- Projetos/Download-music-youtube/source/interface.py
+++ Projetos/Download-music-youtube/source/interface.py
@@ -15,12 +15,15 @@ class Interface():
     def start(self):
         while True:
             try:
-                self.button, self.values = self.janela.Read()
+                self.event, self.values = self.janela.Read()
                 self.clear('output')
                 self.link = self.values['link']
-                DownloadVerfiy(self.link)
+                if self.event == 'Baixar Música':
+                    DownloadVerfiy(self.link)
             except:
                 print("Algo deu errado, portanto o download não pode ser concluido, por favor tente inserir uma nova url!")
+                if self.event == sg.WIN_CLOSED:
+                    break
     
     def clear(self, key):
         self.janela.FindElement(key).Update('')
