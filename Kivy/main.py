a40024d1069c98cb51bef90159b36e8ce95edab7
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-10-08 20:28:03 -0300

    aula 06 full

--- Kivy/main.py
+++ Kivy/main.py
@@ -1,15 +1,15 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 
-class Tarefas(BoxLayout):
+class Tarefas(ScrollView):
     def __init__(self, tarefas, **kwargs):
         super().__init__(**kwargs)
         for tarefa in tarefas :
-            self.add_widget(Label(text=tarefa, font_size=30))
+            self.ids.box.add_widget(Label(text=tarefa, font_size=30, size_hint_y=None, height=200))
 
 class Test(App):
     def build(self):
-        return Tarefas(['Fazer compras', 'Ir para a escola', 'Regar as plantas'], orientation='horizontal')
+        return Tarefas(['Fazer compras', 'Ir para a escola', 'Regar as plantas', 'Cornelius', 'Legal', 'top'])
 
 Test().run()
