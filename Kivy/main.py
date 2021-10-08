ad19ae416538d505e4b5dc58ac9ec5bb27896d61
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-10-08 17:53:13 -0300

    aula 04 full

--- Kivy/main.py
+++ Kivy/main.py
@@ -1,19 +1,10 @@
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 
+class Incrementador(BoxLayout):
+    pass
 class Test(App):
     def build(self):
-        box = BoxLayout(orientation="vertical")
-        button = Button(text="Botão 1", font_size=30, on_release=self.incrementar)
-        self.label = Label(text="1", font_size=30)
-        box.add_widget(self.label)
-        box.add_widget(button)
-
-        return box
-
-    def incrementar(self, button):
-        self.label.text = str(int(self.label.text) + 1)
+        return Incrementador()
 
 Test().run()
