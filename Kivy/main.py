581e6a049e4e583a01931743862dae2a4b54b0d6
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-10-08 17:18:01 -0300

    aula 2 full

--- Kivy/main.py
+++ Kivy/main.py
@@ -1,8 +1,23 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
 class Test(App):
     def build(self):
-        return Button(text='Hello world')
+        box = BoxLayout(orientation="vertical")
+        button = Button(text="Botão 1")
+        label = Label(text="Hello world")
+        box.add_widget(label)
+        box.add_widget(button)
+
+        box2 = BoxLayout()
+        button2 = Button(text="Botão 2")
+        label2 = Label(text="Hello world 2")
+        box2.add_widget(label2)
+        box2.add_widget(button2)
+        
+        box.add_widget(box2)
+        return box
 
 Test().run()
