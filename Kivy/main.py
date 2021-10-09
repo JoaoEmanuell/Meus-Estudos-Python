06ca848af9db38203277209740358e119068531c
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-10-08 21:31:58 -0300

    aula 8 full

--- Kivy/main.py
+++ Kivy/main.py
@@ -1,13 +1,16 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 
-class Tarefas(ScrollView):
+class Tarefas(BoxLayout):
     def __init__(self, tarefas, **kwargs):
         super().__init__(**kwargs)
         for tarefa in tarefas :
             self.ids.box.add_widget(Tarefa(text=tarefa))
+
+    def addWidget(self):
+        texto = self.ids.texto.text
+        self.ids.box.add_widget(Tarefa(text=texto))
+        self.ids.texto.text = ''
 
 class Tarefa(BoxLayout):
     def __init__(self, text='', **kwargs):
