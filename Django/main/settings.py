7634c8072e63a3db91c02f140ca7476341b7a021
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-11-20 11:57:26 -0300

    Key generate

--- Django/main/settings.py
+++ Django/main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from .secret import Key
+from .generate_key import Key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # Chave secreta para autenticação do Django
 key = Key()
-SECRET_KEY = key.getKey()
+SECRET_KEY = key.GetKey()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Caso o debug esteja ativado, o django irá retornar erros que acontecem durante a execução
