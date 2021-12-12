ccecbc7ad0a2d9ae2dfd83f0ce2c728f5408e9a5
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-12-12 11:59:46 -0300

    Aula 3

--- Pyqt5/main.py
+++ Pyqt5/main.py
@@ -1,6 +1,6 @@
 # Imports
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 class window(QMainWindow):
     def __init__(self):
@@ -11,12 +11,27 @@ class window(QMainWindow):
         self.width = 800
         self.height = 600
         self.title = "PyQt5 Window"
+
+        button = self.createButton("Click Me", 100, 100)
+        self.text = 'Hello World'
+        button.clicked.connect(self.buttonClicked)
+
         self.loadWindow()
 
     def loadWindow(self):
         self.setGeometry(self.top, self.left, self.width, self.height)
         self.setWindowTitle(self.title)
         self.show()
+
+    def createButton(self, name, x, y):
+        button = QPushButton(name, self)
+        button.move(x, y)
+        button.resize(100, 100)
+        button.setStyleSheet("background-color: red;font-weight: bold;")
+        return button
+
+    def buttonClicked(self):
+        print(self.text)
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
