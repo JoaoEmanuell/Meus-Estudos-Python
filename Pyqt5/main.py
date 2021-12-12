6d7697d52d6235db4bb551322662601a2be084d8
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-12-12 17:50:07 -0300

    aula 4

--- Pyqt5/main.py
+++ Pyqt5/main.py
@@ -1,6 +1,6 @@
 # Imports
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton ,QLabel
 
 class window(QMainWindow):
     def __init__(self):
@@ -12,9 +12,10 @@ class window(QMainWindow):
         self.height = 600
         self.title = "PyQt5 Window"
 
-        button = self.createButton("Click Me", 100, 100)
+        button = self.CreateButton("Click Me", css="background-color: red; font-weight: bold;")
         self.text = 'Hello World'
         button.clicked.connect(self.buttonClicked)
+        self.label = self.CreateLabel("Hello World", move=(100, 50), css = "color : blue")
 
         self.loadWindow()
 
@@ -23,15 +24,21 @@ class window(QMainWindow):
         self.setWindowTitle(self.title)
         self.show()
 
-    def createButton(self, name, x, y):
+    def CreateButton(self, name='Button', move=(100, 100), resize=(100, 100), **kw):
+        css = kw.get('css', '')
         button = QPushButton(name, self)
-        button.move(x, y)
-        button.resize(100, 100)
-        button.setStyleSheet("background-color: red;font-weight: bold;")
+        button.move(move[0], move[1]), button.resize(resize[0], resize[1]), button.setStyleSheet(css)
         return button
 
+    def CreateLabel(self, text='Label', move=(100, 100), **kw):
+        css = kw.get('css', '')
+        label = QLabel(self)
+        label.setText(text), label.move(move[0], move[1]), label.setStyleSheet(css)
+        return label
+
     def buttonClicked(self):
-        print(self.text)
+        self.label.setText('Button Clicked')
+        self.label.adjustSize()
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
