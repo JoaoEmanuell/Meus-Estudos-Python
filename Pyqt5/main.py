67357ec98916d75d00d46f58dba32f9129bc64cf
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-12-13 16:59:48 -0300

    Aula 05

--- Pyqt5/main.py
+++ Pyqt5/main.py
@@ -1,6 +1,7 @@
 # Imports
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton ,QLabel
+from PyQt5 import QtGui
 
 class window(QMainWindow):
     def __init__(self):
@@ -12,10 +13,14 @@ class window(QMainWindow):
         self.height = 600
         self.title = "PyQt5 Window"
 
-        button = self.CreateButton("Click Me", css="background-color: red; font-weight: bold;")
-        self.text = 'Hello World'
-        button.clicked.connect(self.buttonClicked)
-        self.label = self.CreateLabel("Hello World", move=(100, 50), css = "color : blue")
+        self.button = self.createButton("Hide image", css="background-color: #FF6363; font-weight: bold; border-radius : 15px;")
+        self.button.clicked.connect(self.buttonClicked)
+        self.label = self.createLabel("Hello World", move=(100, 50), css = "color : blue")
+
+        self.image = self.createLabel("", move=(100, 300))
+        self.image.setPixmap(QtGui.QPixmap("car.webp"))
+        self.image.adjustSize()
+        self.showImage = 1
 
         self.loadWindow()
 
@@ -24,21 +29,30 @@ class window(QMainWindow):
         self.setWindowTitle(self.title)
         self.show()
 
-    def CreateButton(self, name='Button', move=(100, 100), resize=(100, 100), **kw):
+    def createButton(self, name='Button', move=(100, 100), resize=(100, 100), **kw):
         css = kw.get('css', '')
         button = QPushButton(name, self)
         button.move(move[0], move[1]), button.resize(resize[0], resize[1]), button.setStyleSheet(css)
         return button
 
-    def CreateLabel(self, text='Label', move=(100, 100), **kw):
+    def createLabel(self, text='Label', move=(100, 100), **kw):
         css = kw.get('css', '')
         label = QLabel(self)
         label.setText(text), label.move(move[0], move[1]), label.setStyleSheet(css)
         return label
 
     def buttonClicked(self):
+        images_list = ['car.webp', '']
         self.label.setText('Button Clicked')
-        self.label.adjustSize()
+        self.image.setPixmap(QtGui.QPixmap(images_list[self.showImage]))
+        self.image.adjustSize()
+        
+        if self.showImage == 0:
+            self.showImage = 1
+            self.button.setText("Hide image")
+        else:
+            self.showImage = 0
+            self.button.setText("Show image")
 
 if __name__=="__main__":
     app = QApplication(sys.argv)
