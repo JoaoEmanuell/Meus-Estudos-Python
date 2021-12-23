bea2cbd2d8cda859ab61ca46e5879fb88a35d779
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-12-23 16:04:13 -0300

    linhas de comentario login_account.py

--- Projetos/CRUD/source/login_account.py
+++ Projetos/CRUD/source/login_account.py
@@ -8,6 +8,15 @@ class Login(sql_essential.sql_essential):
         self.connection = self.create_connection()
 
     def login_account(self, name, password):
+        """[Login to account]
+
+        Args:
+            name ([str]): [User name]
+            password ([str]): [Password]
+
+        Returns:
+            [tuple, None]: [If the login is valid it returns a tuple with the username and password, if not, it returns None]
+        """
         cursor = self.connection.cursor()
         sql = f"SELECT NAME, PASS FROM TEST WHERE NAME = '{name}' AND PASS = '{password}'"
         cursor.execute(sql)
