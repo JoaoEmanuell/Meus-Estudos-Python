08422bcbf7f35f8dc85f501ebefa668610ebf74d
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-01-17 15:15:52 -0300

    user_information_control set_informations_in_list and template_color_center

--- Projetos/GitHub_user_information/source/layouts_controls/user_information_control.py
+++ Projetos/GitHub_user_information/source/layouts_controls/user_information_control.py
@@ -9,21 +9,25 @@ class user_information_control():
     def show_user_information(self) -> None:
         self.user_informations.show()
         self.user_informations.user_list_informations.clear()
-        self.user_informations.username.setText(f'<html><head/><body><p><span style = "color : #8b949e">{self.user["login"]}</span></p></body></html>')
-        self.user_informations.user_name.setText(f'<html><head/><body><p><span style = "color : #C9D1D9">{self.user["name"]}</span></p></body></html>')
-        self.user_informations.user_bio.setText(f'<html><head/><body><p><span style = "color : #C9D1D9">{self.user["bio"]}</span></p></body></html>')
-        location_icon = QIcon(join(Path().absolute(), 'icons/location_icon.png'))
-        followers_icon = QIcon(join(Path().absolute(), 'icons/followers_icon.png'))
-        followers_item = QListWidgetItem(followers_icon, f"{self.user['followers']} Seguidores | Seguindo {self.user['following']}")
-        location_item = QListWidgetItem(location_icon, self.user["location"])
-        self.user_informations.user_list_informations.addItem(followers_item)
-        self.user_informations.user_list_informations.addItem(location_item)
+        template_color_center = '<p><span style="color:%s">%s</span></p>'
+        self.user_informations.username.setText(template_color_center % ('#8b949e', self.user['login']))
+        self.user_informations.user_name.setText(template_color_center % ('#C9D1D9', self.user['name']))
+        self.user_informations.user_bio.setText(template_color_center % ('#C9D1D9', self.user['bio']))
+        user_information_control.set_informations_in_list(self)
         user_information_control.image_set(self)
 
     def image_set(self):
         image = QImage()
         image.loadFromData(get(self.user['avatar_url']).content)
         self.user_informations.user_profile_photo.setPixmap(QPixmap(image).scaled(250, 250))
+
+    def set_informations_in_list(self):
+        location_icon = QIcon(join(Path().absolute(), 'icons/location_icon.png'))
+        followers_icon = QIcon(join(Path().absolute(), 'icons/followers_icon.png'))
+        followers_item = QListWidgetItem(followers_icon, f"{self.user['followers']} Seguidores | Seguindo {self.user['following']}")
+        location_item = QListWidgetItem(location_icon, self.user["location"])
+        self.user_informations.user_list_informations.addItem(followers_item)
+        self.user_informations.user_list_informations.addItem(location_item)
     
     @lru_cache
     def get_user_repos(self) : return get(self.user['repos_url']).json()
