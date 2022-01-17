e22362deb91ff2909438edb927cf3b826c7c3d85
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-01-17 15:51:12 -0300

    User 0 repos bug fixed

--- Projetos/GitHub_user_information/source/layouts_controls/repos_name_control.py
+++ Projetos/GitHub_user_information/source/layouts_controls/repos_name_control.py
@@ -1,8 +1,13 @@
+from PyQt5.QtWidgets import QMessageBox
 class repos_name_control():
     def set_repos_in_list(self):
-        self.repos_name.list_repos_names.clear()
-        for repo in self.repos:
-            self.repos_name.list_repos_names.addItem(repo['name'])
+        if len(self.repos) == 0:
+            QMessageBox.about(self.user_informations, 'Erro', 'The desired profile has no accessible repositories!')
+            self.app.closeAllWindows(), self.user_informations.show()
+        else :
+            self.repos_name.list_repos_names.clear()
+            for repo in self.repos:
+                self.repos_name.list_repos_names.addItem(repo['name'])
 
     def show_repo_info(self):
         self.repos_name.close()
