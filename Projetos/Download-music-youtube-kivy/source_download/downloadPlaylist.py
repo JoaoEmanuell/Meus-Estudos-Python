11b7b5d83487c561abc4611013300c0b555b12ad
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-03-15 11:29:55 -0300

    DownloadPlaylist interact with DownloadInterface

--- Projetos/Download-music-youtube-kivy/source_download/downloadPlaylist.py
+++ Projetos/Download-music-youtube-kivy/source_download/downloadPlaylist.py
@@ -3,20 +3,23 @@
 from pytube import Playlist
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+from typing import Type
 
 # Local imports
 
-from .downloadVideo import DownloadVideo
+from .interfaces import DownloadPlaylistInterface, DownloadInterface
 from .message import Message
 
-class DownloadPlaylist():
-    def __init__(self, playlistLink : list, convertToMp3  : bool = True) -> None:
-        self.playlist = Playlist(playlistLink)
-        self.CONVERT = convertToMp3
-        DownloadPlaylist.downloadVideos(self)
+class DownloadPlaylist(DownloadPlaylistInterface):
+    def __init__(self, playlistLink : list, convertToMp3  : bool) -> None:
+        self.__playlist = Playlist(playlistLink)
+        self.__CONVERT = convertToMp3
 
-    def downloadVideos(self):
+    def download_playlist(self, download : Type[DownloadInterface]):
         Message.set_output(f"Download da playlist iniciado!")
-        for v in self.playlist:
-            DownloadVideo(v, self.CONVERT)
+        for video in self.__playlist:
+            if self.__CONVERT:
+                download(video, self.__CONVERT).downloadAudio()
+            else:
+                download(video, self.__CONVERT).downloadVideo()
         Message.set_output("Download da Playlist concluido!")
