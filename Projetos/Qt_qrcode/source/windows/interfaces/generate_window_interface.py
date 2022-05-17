b486fd363489eafa4f4110801b3effdc76aacd96
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-05-17 20:16:15 -0300

    Generate window interface docstrings

--- Projetos/Qt_qrcode/source/windows/interfaces/generate_window_interface.py
+++ Projetos/Qt_qrcode/source/windows/interfaces/generate_window_interface.py
@@ -4,14 +4,39 @@ from qrcode.image.base import BaseImage
 from qrcode.image.pil import PilImage
 
 class GenerateWindowInterface(ABC) : 
+    """GenerateWindowInterface
+
+    Args:
+        ABC (_type_): _description_
+    """    
     @abstractmethod
     def __init__(self, url : str) -> None:
+        """Init
+
+        Args:
+            url (str): Url to generate QR code
+        """        
         raise NotImplementedError()
 
     @abstractmethod
     def generate_qrcode(self) -> Union[PilImage, BaseImage]:
+        """Generate QR code
+
+        Returns:
+            Union[PilImage, BaseImage]: QR code image
+        """        
         raise NotImplementedError()
 
     @abstractmethod
     def save_image(self, image : Union[PilImage, BaseImage], path : str) -> None:
+        """Save QR code image
+
+        Args:
+            image (Union[PilImage, BaseImage]): QR code image
+            path (str): Path to save image
+
+        Raises:
+            ValueError: If file extension is invalid (not .png)
+        """        
         raise NotImplementedError()
+        raise ValueError
