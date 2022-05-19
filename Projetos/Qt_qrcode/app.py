d355ba8f6e7340a88cf8790319ec92e01c305200
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-05-19 12:07:30 -0300

    Create and clear tmp directory

--- Projetos/Qt_qrcode/app.py
+++ Projetos/Qt_qrcode/app.py
@@ -1,4 +1,25 @@
+from os.path import join
+from os import listdir, remove
+from pathlib import Path
+
 from source import MainWindow
 
+def create_tmp() :
+    """Create a temporary directory"""    
+    path = Path().absolute()
+    path = join(path, '.tmp')
+    if not Path(path).exists() :
+        Path(path).mkdir()
+
+def clear_tmp() :
+    """Clear the temporary directory"""
+    path = Path().absolute()
+    path = join(path, '.tmp', '')
+    if Path(path).exists() :
+        for file in listdir(path) :
+            remove(f'{path}{file}')
+
 if __name__ == '__main__' :
+    create_tmp()
     window = MainWindow()
+    clear_tmp()
